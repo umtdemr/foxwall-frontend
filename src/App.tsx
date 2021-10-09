b421d17843c85c047994fe1535896e470b6e4be4
umtdemr <umitde692@gmail.com>
2021-10-09 11:43:40 +0300

    added base homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { AuthActionProvider } from './modules/contexts/auth/auth.context';
 import AuthPage from './pages/auth/auth-page';
+import HomePage from './pages/home/home-page';
 
 
 function App() {
   return (
-    <AuthActionProvider>
-      <AuthPage />
-    </AuthActionProvider>
+    // <AuthActionProvider>
+    //   <AuthPage />
+    // </AuthActionProvider>
+    <HomePage />
   );
 }
 
---
+++ src/pages/home/home-page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const HomePage: React.FC = () => {
+    return (
+        <div>This is homepage</div>
+    );
+}
+
+
+export default HomePage;
