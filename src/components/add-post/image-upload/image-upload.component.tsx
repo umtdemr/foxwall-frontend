5caeafcce6c700f18c5135994554ac5125c68fb1
umtdemr <umitde692@gmail.com>
2021-10-12 22:39:08 +0300

    added upload image preview chips

--- src/components/add-post/image-upload/image-upload.component.tsx
+++ src/components/add-post/image-upload/image-upload.component.tsx
@@ -1,24 +1,48 @@
 import React from "react";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import { InsertPhoto } from "@mui/icons-material";
 import { blue } from "@mui/material/colors";
-
+import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
+import { Avatar, Chip, Box } from "@mui/material";
 
 const Input = styled("input")({
   display: "none",
 });
 
 const ImageUploadPostForm: React.FC = () => {
+  const handleDelete = () => {
+    console.log("Delte");
+  };
   return (
-    <label htmlFor="icon-button-file">
-      <Input accept="image/*" id="icon-button-file" type="file" multiple />
-      <IconButton color="primary" aria-label="upload picture" component="span">
-        <InsertPhoto sx={{ color: blue[200] }} />
-      </IconButton>
-    </label>
+    <Stack direction="row">
+      <Stack spacing={1} direction="row" alignItems="center">
+        <Box sx={{ position: "relative" }}>
+          <Chip
+            onDelete={handleDelete}
+            avatar={
+              <Avatar
+                src={
+                  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                }
+                variant="square"
+              ></Avatar>
+            }
+            label="qweqew.jpg"
+          ></Chip>
+        </Box>
+      </Stack>
+      <label htmlFor="icon-button-file">
+        <Input accept="image/*" id="icon-button-file" type="file" multiple />
+        <IconButton
+          color="primary"
+          aria-label="upload picture"
+          component="span"
+        >
+          <InsertPhoto sx={{ color: blue[200] }} />
+        </IconButton>
+      </label>
+    </Stack>
   );
 };
 
