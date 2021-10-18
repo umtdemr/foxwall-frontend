32b29e80cffee8c53dd2100f83ce4b39ed5378d4
umtdemr <umitde692@gmail.com>
2021-10-18 16:07:02 +0300

    added search and follow request base designs

--- src/components/sidebar/sidebar_component.tsx
+++ src/components/sidebar/sidebar_component.tsx
@@ -1,7 +1,45 @@
 import React from "react";
 
+import { Button, Divider, Chip, Stack, ListItem, List, ListItemAvatar, Avatar, ListItemText, IconButton } from "@mui/material";
+import { Search, Check, Remove } from "@mui/icons-material"
+
 const SideBar: React.FC = () => {
-  return <div>Sidebar contents here...</div>;
+  return (
+    <>
+      <Button variant="outlined" startIcon={<Search />} sx={{ display: "flex", margin: "10px auto 0 auto;", textTransform: "lowercase" }}>
+        search users...
+      </Button>
+      <Stack direction="column">
+        <Divider sx={{ marginTop: "20px" }}>
+          <Chip label="requests" />
+        </Divider>
+        <List >
+          <ListItem
+            secondaryAction={
+              <>
+                <IconButton color="error" edge="end" aria-label="remove" sx={{ marginRight: "2px" }}>
+                  <Remove />
+                </IconButton>
+                <IconButton color="success" edge="end" aria-label="accept">
+                  <Check />
+                </IconButton>
+              </>
+            }
+          >
+            <ListItemAvatar>
+              <Avatar
+                src="https://www.pngrepo.com/png/9649/512/avatar.png"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Ümit Demir"
+            />
+          </ListItem>
+        </List>
+
+      </Stack>
+    </>
+  );
 };
 
 export default SideBar;
