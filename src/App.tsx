97157aa2a7b561327cf7630f5f63d4600c879e6c
umtdemr <umitde692@gmail.com>
2021-10-23 19:40:20 +0300

    added basic route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchModal from "./components/search-modal/search-modal.component";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthActionProvider } from "./modules/contexts/auth/auth.context";
 import AuthPage from "./pages/auth/auth-page";
 import HomePage from "./pages/home/home-page";
@@ -9,10 +10,16 @@ function App() {
     // <AuthActionProvider>
     //   <AuthPage />
     // </AuthActionProvider>
-    <>
-      <HomePage />
+    <div className="content">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
       <SearchModal />
-    </>
+    </div>
   );
 }
 
