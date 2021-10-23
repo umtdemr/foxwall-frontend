2f39996fb0babc556793914334b48f997094016e
umtdemr <umitde692@gmail.com>
2021-10-23 15:24:47 +0300

    added dev dependencies for linting

--- src/components/sidebar/sidebar_component.tsx
+++ src/components/sidebar/sidebar_component.tsx
@@ -7,10 +7,10 @@ import RequestItems from "../requests/requests.component";
 const SideBar: React.FC = () => {
   return (
     <>
-      <Button color="primary" variant="outlined" startIcon={<Search />} sx={{ display: "flex", margin: "10px auto 0 auto;", textTransform: "lowercase", borderColor: "#999", color: "#666" }}>
+        <Button color="primary" variant="outlined" startIcon={<Search />} sx={{ display: "flex", margin: "10px auto 0 auto;", textTransform: "lowercase", borderColor: "#999", color: "#666" }}>
         search users...
       </Button>
-      <RequestItems />
+        <RequestItems />
     </>
   );
 };
