3488b3a29fa25140cafd0b966c4e257f839dc469
umtdemr <umitde692@gmail.com>
2021-11-08 19:06:39 +0300

    added custom switch button

--- src/components/sidebar/sidebar_component.tsx
+++ src/components/sidebar/sidebar_component.tsx
@@ -1,37 +1,39 @@
 import React from "react";
 import { Search } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleSearchPopup } from "../../redux/slices/layout";
 import type { ILayoutState } from "../../redux/slices/layout";
 import type { RootState } from "../../redux/store";
 import RequestItems from "../requests/requests.component";
 import { ColorModeContext } from "../../modules/contexts/theme/theme.context";
+import SwitchButton from "../swith-button/switch-button";
 
 const SideBar: React.FC = () => {
   const state: ILayoutState = useSelector((state: RootState) => state.layout);
   const dispatch = useDispatch();
   const colorMode = React.useContext(ColorModeContext);
   return (
-    <>
-      <Button
-        color="primary"
-        variant="outlined"
-        startIcon={<Search />}
-        sx={{
-          display: "flex",
-          margin: "10px auto 0 auto;",
-          textTransform: "lowercase",
-          borderColor: "#999",
-          color: "#666",
-        }}
-        onClick={() => dispatch(toggleSearchPopup())}
-      >
-        search users...
-      </Button>
+    <div className="sidebar" style={{ padding: "10px" }}>
+      <Stack direction="row" justifyContent="space-between">
+        <Button
+          color="primary"
+          variant="outlined"
+          startIcon={<Search />}
+          sx={{
+            textTransform: "lowercase",
+            borderColor: "#999",
+            color: "#666",
+          }}
+          onClick={() => dispatch(toggleSearchPopup())}
+        >
+          search users...
+        </Button>
+        <SwitchButton />
+      </Stack>
       <RequestItems />
       <Button onClick={() => colorMode.toggleColorMode()}>DEMO THEME</Button>
-    </>
+    </div>
   );
 };
 
