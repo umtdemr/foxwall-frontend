d95956a579e276a88b57dcb06b9e27223c5e54ce
umtdemr <umitde692@gmail.com>
2021-11-14 21:14:16 +0300

    layout options for responsive design

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,28 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import {
-    Container,
-    Grid,
-} from "@mui/material";
-import SideBar from '../sidebar/sidebar_component';
-import HeadWithLogo from '../head/head-with-logo.component';
+import { Container, Grid } from "@mui/material";
+import SideBar from "../sidebar/sidebar_component";
+import HeadWithLogo from "../head/head-with-logo.component";
 
-
-const Layout: React.FC = ({children}) => {
-    return (
-        <Container>
-            <Grid container justifyContent="center" spacing={2}>
-                <Grid item xs={6}>
-                    <HeadWithLogo />
-                    {children}
-                </Grid>
-                <Grid item xs={4}>
-                    <SideBar />
-                </Grid>
-            </Grid>
-        </Container>
-    )
-}
-
+const Layout: React.FC = ({ children }) => {
+  return (
+    <Container>
+      <Grid container justifyContent="center" spacing={2}>
+        <Grid item md={6} xs={12}>
+          <HeadWithLogo />
+          {children}
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <SideBar />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default Layout;
