a1bc36902a09819cce148b62b0d7e350e64eac31
umtdemr <umitde692@gmail.com>
2021-11-17 22:45:47 +0300

    deleted uneccessary imports from sidebr

--- src/components/sidebar/sidebar_component.tsx
+++ src/components/sidebar/sidebar_component.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Search } from "@mui/icons-material";
 import { Button, Stack } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { toggleSearchPopup } from "../../redux/slices/layout";
-import type { ILayoutState } from "../../redux/slices/layout";
-import type { RootState } from "../../redux/store";
 import RequestItems from "../requests/requests.component";
 import { ColorModeContext } from "../../modules/contexts/theme/theme.context";
 import SwitchButton from "../swith-button/switch-button";
 
 const SideBar: React.FC = () => {
-  const state: ILayoutState = useSelector((state: RootState) => state.layout);
   const dispatch = useDispatch();
   const colorMode = React.useContext(ColorModeContext);
 
