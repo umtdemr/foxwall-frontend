15e047540fb505767e942b9098daf5ff47829b49
umtdemr <umitde692@gmail.com>
2021-12-09 20:29:17 +0300

    added formik ✌️

--- src/components/auth-form/register/register-form.component.tsx
+++ src/components/auth-form/register/register-form.component.tsx
@@ -3,11 +3,20 @@ import React, { useState } from "react";
 import { TextField, Button } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { useDispatch } from "react-redux";
+import { Formik, Form, Field, FieldAttributes, useField } from "formik";
 
 import "../form.css";
 import { authActionTypes } from "../../../types/auth/auth-types";
 import { AuthActionContext } from "../../../modules/contexts/auth/auth.context";
 import { registerThunk } from "../../../redux/slices/auth/auth-thunks";
+
+
+const TextFieldFormik: React.FC<FieldAttributes<{}>> = ({...props }) => {
+  const [field, meta] = useField(props);
+  const errorText = meta.error && meta.touched ? meta.error : '';
+  return <TextField {...field} helperText={errorText} error/>
+}
+
 
 const RegisterForm: React.FC = () => {
   const [name, setName] = useState("");
@@ -27,40 +36,72 @@ const RegisterForm: React.FC = () => {
     }));
   }
 
+
   return (
-    <form>
-      <TextField 
-        className="form_field full" 
-        label="Name" 
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />
-      <TextField 
-        className="form_field full" 
-        label="email" 
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <TextField 
-        className="form_field full" 
-        label="username"
-        onChange={(e) => setUsername(e.target.value)}
-        value={username}
-      />
-      <TextField 
-        className="form_field full" 
-        label="password" 
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <Button variant="outlined" onClick={() => handleRegister()}>Sign up</Button>
+    <div>
+      <Formik 
+        initialValues={{ 
+          name: '',
+          email: '',
+          username: '',
+          password: '',
+        }}
+        onSubmit={(data, {setSubmitting}) => {
+          setSubmitting(true);
+          // make some async works here...
+          console.log("Submit : ", data);
+          setSubmitting(false);
+        }
+      }>
+        { ({
+          values,
+          isSubmitting,
+          handleBlur,
+          handleChange,
+        }) => (
+          <Form>
+            <TextFieldFormik
+              name="name"
+              className="form_field full" 
+              type="input"
+            />
+            <Field
+              name="email"
+              label="Email"
+              className="form_field full" 
+              type="mail"
+              as={TextField}
+            />
+            <Field
+              name="username"
+              label="Username"
+              className="form_field full" 
+              type="input"
+              as={TextField}
+            />
+            <Field
+              name="password"
+              label="Password"
+              className="form_field full" 
+              type="password"
+              as={TextField}
+            />
+            <Button variant="outlined" type="submit" disabled={isSubmitting}>Sign up</Button>
+            <pre>
+              {
+                JSON.stringify(values, null, 2)
+              }
+            </pre>
+          </Form>
+        )}
+      </Formik>
       <span className="form_have_not_text" style={{color: theme.palette.text.primary}}>
         Do you have an account?
         <button onClick={() => changeType!(authActionTypes.LOGIN)}>
           Sign in.
         </button>
       </span>
-    </form>
+    </div>
   );
 };
 
