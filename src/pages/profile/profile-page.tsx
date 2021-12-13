8fbf5bf8d09244bfbc48551c07ab251bbd954f2e
umtdemr <umitde692@gmail.com>
2021-12-13 21:12:31 +0300

    started to fetch profile ▷

--- src/pages/profile/profile-page.tsx
+++ src/pages/profile/profile-page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 import PostList from '../../components/post/post-list.component';
 import ProfileHeader from '../../components/profile/profile-header/profile-header.component';
 
 
 const ProfilePage: React.FC = () => {
+    const { username } = useParams<{username?: string}>();
+
+    useEffect(() => {
+       // fetch profile data
+    }, [])
     return (
         <div>
             <ProfileHeader />
