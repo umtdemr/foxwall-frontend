58b3e5f2c72a44b332fe97e437f23c43b9258586
umtdemr <umitde692@gmail.com>
2021-12-16 22:51:47 +0300

    fixed input color issue on auth page

--- src/components/auth-form/text-formik-field/text-formik-field.component.tsx
+++ src/components/auth-form/text-formik-field/text-formik-field.component.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 
 import { TextField } from "@material-ui/core";
 import { FieldAttributes, useField } from "formik";
+import { useTheme } from '@emotion/react';
 
 
 const TextFieldFormik: React.FC<FieldAttributes<{}>> = ({placeholder, className, type="text",   ...props }) => {
   const [field, meta] = useField(props);
+  const theme: any = useTheme();
   const errorText = meta.error && meta.touched ? meta.error : '';
   return <TextField 
     {...field}
     label={placeholder} 
-    className={className}
+    className={`${className} ${theme.palette.mode}`}
     helperText={errorText} 
     error={!!errorText}
     type={type}
+    style={{
+      color: 'red !important'
+    }}
   />
 }
 
