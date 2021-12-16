71a9af7bc0e0d02b9793610b92b2ce50adc74bd2
umtdemr <umitde692@gmail.com>
2021-12-16 21:04:30 +0300

    fixed persist state parse error 😏

--- src/modules/hooks/persist-state.tsx
+++ src/modules/hooks/persist-state.tsx
@@ -3,8 +3,6 @@ import React, { useState, useEffect } from "react";
 function usePersistedState<T>(key: string, defaultValue: T) {
   const [state, setState] = useState(() => {
     const storedValue = localStorage.getItem(key);
-    console.log("STORED Val: ", storedValue);
-    console.log(storedValue === null);
     return (storedValue !== null && storedValue !== "") 
       ? JSON.parse(storedValue)
       : defaultValue;
