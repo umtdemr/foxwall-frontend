0fc7f754154b713045a6a631e2ff352b2b3de7eb
umtdemr <umitde692@gmail.com>
2022-01-24 23:10:08 +0300

    created list search items

--- src/components/search-modal/search-modal.component.tsx
+++ src/components/search-modal/search-modal.component.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Box, Modal, Stack, TextField, Typography } from "@mui/material";
+import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Modal, Stack, TextField, Typography } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { ILayoutState, toggleSearchPopup } from "../../redux/slices/layout";
 import { RootState } from "../../redux/store";
+import { useTheme } from "@mui/material/styles";
 
 import { boxStyle } from "./search-model.style";
-
+import { Link } from "react-router-dom";
 
 
 const SearchModal: React.FC = () => {
   const state: ILayoutState = useSelector((state: RootState) => state.layout);
   const dispatch = useDispatch();
+  const theme = useTheme();
+
   return (
     <Modal
       open={state.isSearchPopupOpen}
@@ -24,6 +27,27 @@ const SearchModal: React.FC = () => {
         </Typography>
         <Stack>
           <TextField placeholder="username, name..." />
+        </Stack>
+        <Stack>
+          <List>
+            <ListItem>
+              <Link to="/">
+                <ListItemAvatar>
+                  <Avatar
+                    src="https://www.pngrepo.com/png/9649/512/avatar.png"
+                  />
+                </ListItemAvatar>
+              </Link>
+              <Link to="/">
+                <ListItemText
+                    primary="John doe"
+                    sx={{
+                      color: theme.palette.text.primary,
+                    }}
+                />
+              </Link>
+            </ListItem>
+          </List>
         </Stack>
       </Box>
     </Modal>
