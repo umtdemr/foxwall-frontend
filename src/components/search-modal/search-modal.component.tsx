44c0a87c46e914c801bae81813d82ca6b7bd174c
umtdemr <umitde692@gmail.com>
2022-01-25 18:22:47 +0300

    created searc h

--- src/components/search-modal/search-modal.component.tsx
+++ src/components/search-modal/search-modal.component.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Modal, Stack, TextField, Typography } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { ILayoutState, toggleSearchPopup } from "../../redux/slices/layout";
@@ -6,11 +6,28 @@ import { RootState } from "../../redux/store";
 
 import { boxStyle } from "./search-model.style";
 import SearchResults from "./search-results/search-results.component";
+import { useAuthenticatedAPI } from "../../modules/api/api";
 
 
 const SearchModal: React.FC = () => {
   const state: ILayoutState = useSelector((state: RootState) => state.layout);
+  const [query, setQuery] = useState("");
   const dispatch = useDispatch();
+  const API = useAuthenticatedAPI();
+
+  useEffect(() => {
+    const searchFromAPI = async () => {
+      const response = await API.get(`/user/search/?q=${query}`);
+    }
+    const delayedApiSearch = setTimeout(() => {
+      if (query !== "" && query.length > 0) {
+        searchFromAPI();
+      }
+    }, 1000);
+
+    return () => clearTimeout(delayedApiSearch);
+  }, [query, API])
+
 
   return (
     <Modal
@@ -24,7 +41,11 @@ const SearchModal: React.FC = () => {
           Search from foxwallers
         </Typography>
         <Stack>
-          <TextField placeholder="username, name..." />
+          <TextField 
+            placeholder="username, name..." 
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </Stack>
         <SearchResults /> 
       </Box>
