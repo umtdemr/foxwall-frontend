5041ac739f94760135aeec7c713631c7309d1d8b
umtdemr <umitde692@gmail.com>
2022-01-25 18:38:14 +0300

    search modal is closing on route change

--- src/components/search-modal/search-results/search-results.component.tsx
+++ src/components/search-modal/search-results/search-results.component.tsx
@@ -3,6 +3,8 @@ import { useTheme } from "@mui/material/styles";
 import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Stack, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { UserProfile } from '../../../types/global/profile_types';
+import { useDispatch } from 'react-redux';
+import { toggleSearchPopup } from '../../../redux/slices/layout';
 
 
 interface SearchResultsProps {
@@ -12,22 +14,26 @@ interface SearchResultsProps {
 
 const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
     const theme = useTheme();
+    const dispatch = useDispatch();
     return (
         <Stack>
             {
                 results.length > 0 && <List sx={{maxHeight: "204px", overflowY: "auto", marginTop: "15px"}}>
                     {
                         results.map(result => <ListItem>
-                            <Link to={`/profile/${result.username}`}>
+                            <Link to={`/profile/${result.username}`} onClick={() => dispatch(toggleSearchPopup())}>
                                 <ListItemAvatar>
                                 <Avatar
                                     src={result.profile.avatar}
                                 />
                                 </ListItemAvatar>
                             </Link>
-                            <Link to={`/profile/${result.username}`} style={{
+                            <Link 
+                                to={`/profile/${result.username}`} 
+                                style={{
                                     textDecoration: "none",
                                 }} 
+                                onClick={() => dispatch(toggleSearchPopup())}
                             >
                                 <ListItemText
                                     primary={result.profile.name}
