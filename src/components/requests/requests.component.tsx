3271322ab90d4bda6f802373a3e6d445453b698e
umtdemr <umitde692@gmail.com>
2022-01-26 23:19:44 +0300

    removed request label if no request has come & multiple skeleton

--- src/components/requests/requests.component.tsx
+++ src/components/requests/requests.component.tsx
@@ -15,9 +15,11 @@ const RequestItems: React.FC = () => {
 
   return (
     <Stack direction="column">
-      <Divider sx={{ marginTop: "20px" }}>
-        <Chip label="requests" />
-      </Divider>
+      {
+        state.results.length > 0 && <Divider sx={{ marginTop: "20px" }}>
+          <Chip label="requests" />
+        </Divider>
+      }
       <List>
         <TransitionGroup>
           {
@@ -30,7 +32,7 @@ const RequestItems: React.FC = () => {
           }
         </TransitionGroup>
         {
-          state.loading && <RequestItemSkeleton />
+          state.loading && new Array(2).fill(2).map(_ => <RequestItemSkeleton />)
         }
       </List>
     </Stack>
