c9815e02ebe633bc872da6effcf4ded0bb5613f1
umtdemr <umitde692@gmail.com>
2022-01-28 01:17:14 +0300

    relative time coded replaced with modern Intl Class

--- src/modules/utils/relative_time.ts
+++ src/modules/utils/relative_time.ts
@@ -1,37 +1,27 @@
 export default function timeDifference(createdAt: string) {
-
-    const current = new Date().getTime();
-    const previous = new Date(createdAt).getTime();
-
-    const msPerMinute = 60 * 1000;
-    const msPerHour = msPerMinute * 60;
-    const msPerDay = msPerHour * 24;
-    const msPerMonth = msPerDay * 30;
-    const msPerYear = msPerDay * 365;
-
-    const elapsed = (current - previous);
-
-    if (elapsed < msPerMinute) {
-        return Math.round(elapsed/1000) + ' seconds ago';   
+    const units: any = {
+        year: 24 * 60 * 60 * 1000 * 365,
+        month: 24 * 60 * 60 * 1000 * 365/12,
+        day: 24 * 60 * 60 * 1000,
+        hour: 60 * 60 * 1000,
+        minute: 60 * 1000,
+        second: 1000,
+    }
+  
+    const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
+  
+    const getRelativeTime = (d1: Date, d2: Date = new Date()) => {
+        const elapsed = d1.getTime() - d2.getTime()
+  
+        // "Math.abs" accounts for both "past" & "future" scenarios
+        for (const unit of Object.keys(units)) {
+            type u_types = "year" | "month" | "day" | "hour" | "minute" | "second";
+            const timeUnit = unit as u_types;
+    
+            if (Math.abs(elapsed) > units[unit] || unit === 'second')
+                return rtf.format(Math.round(elapsed/units[unit]), timeUnit)
+        }
     }
 
-    else if (elapsed < msPerHour) {
-        return Math.round(elapsed/msPerMinute) + ' minutes ago';   
-    }
-
-    else if (elapsed < msPerDay ) {
-        return Math.round(elapsed/msPerHour ) + ' hours ago';   
-    }
-
-    else if (elapsed < msPerMonth) {
-        return Math.round(elapsed/msPerDay) + ' days ago';   
-    }
-
-    else if (elapsed < msPerYear) {
-        return Math.round(elapsed/msPerMonth) + ' months ago';   
-    }
-
-    else {
-        return Math.round(elapsed/msPerYear ) + ' years ago';   
-    }
+    return getRelativeTime(new Date(createdAt));
 }
