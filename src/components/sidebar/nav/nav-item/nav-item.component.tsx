b82487e381d81aa317187fd22c7a9c61920f79e4
umtdemr <umitde692@gmail.com>
2022-02-10 15:33:54 +0300

    added nav items

--- src/components/sidebar/nav/nav-item/nav-item.component.tsx
+++ src/components/sidebar/nav/nav-item/nav-item.component.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
-import { Home } from '@mui/icons-material';
 import { useTheme } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
 
-const NavItem: React.FC = () => {
+interface NavItemProps {
+    Icon: React.ReactChild;
+    link: string;
+    text: string;
+}
+
+const NavItem: React.FC<NavItemProps> = ({ Icon, link, text }) => {
     const theme = useTheme();
     return (
-        <Link to="/" className="nav_item__link">
+        <Link 
+            to={link} 
+            className="nav_item__link"
+        >
             <Stack 
                 direction="row"
                 alignItems="center"
                 className="nav_item__wrapper"
             >
-                <Home sx={{
-                    width: 36,
-                    height: 36,
-                    color: theme.palette.text.primary,
-                }}/>
-                <Typography variant='h6' sx={{
+                {Icon}
+                <Typography variant='h6' ml={1} sx={{
                     color: theme.palette.text.primary,
                     textDecoration: 'none',
                 }}>
-                    Home
+                    {text}
                 </Typography>
             </Stack>
         </Link>        
--- src/components/sidebar/nav/nav.component.tsx
+++ src/components/sidebar/nav/nav.component.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import { Stack } from '@mui/material';
+import { Home, Person, Logout } from '@mui/icons-material';
+import { useTheme } from '@mui/material/styles';
 import NavItem from './nav-item/nav-item.component';
+import { IUserInitialState } from '../../../redux/slices/user';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
+
 
 const SidebarNav: React.FC = () => {
+    const theme = useTheme();
+    const state: IUserInitialState = useSelector((state: RootState) => state.user);
+
     return (
         <Stack mt={2}>
-            <NavItem />
-            <NavItem />
-            <NavItem />
+            <NavItem 
+                Icon={<Home sx={{ 
+                    width: 32,
+                    height: 32,
+                    color: theme.palette.text.primary,
+                }}/>} 
+                link="/"
+                text="Home"
+            />
+            <NavItem 
+                Icon={<Person sx={{ 
+                    width: 32,
+                    height: 32,
+                    color: theme.palette.text.primary,
+                }}/>} 
+                link={`/profile/${state.username}`}
+                text="Profile"
+            />
+            <NavItem 
+                Icon={<Logout sx={{ 
+                    width: 32,
+                    height: 32,
+                    color: theme.palette.text.primary,
+                }}/>} 
+                link="/"
+                text="Logout"
+            />
         </Stack>
     )
 }
