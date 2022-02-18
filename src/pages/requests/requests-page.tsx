07b478e8c3c9825ec90afdca7dcbee05b2cd3b10
umtdemr <umitde692@gmail.com>
2022-02-18 15:04:29 +0300

    finished request page funcitonality

--- src/pages/requests/requests-page.tsx
+++ src/pages/requests/requests-page.tsx
@@ -1,10 +1,24 @@
+import { Typography } from '@material-ui/core';
+import { Stack } from '@mui/material';
 import React from 'react'
+import { useSelector } from 'react-redux';
 import RequestItems from '../../components/requests/requests.component';
+import { IRequestsData } from '../../redux/slices/requests';
+import { RootState } from '../../redux/store';
 
 
 const RequestsPage: React.FC = () => {
+    const state: IRequestsData = useSelector((state: RootState) => state.requests);
     return (
-        <RequestItems />
+        <Stack>
+
+            <RequestItems />
+            {
+                state.results.length < 1 && <Typography align='center'>
+                    No requests
+                </Typography>
+            }
+        </Stack>
     )
 }
 
